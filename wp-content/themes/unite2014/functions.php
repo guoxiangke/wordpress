7e923691ea04ac79cf95e0071ed80804449afa8c
Dale@profero <Dale.Guo@profero.com>
2014-08-08 23:05:25 +0800

    add nexgen-gallery

--- wp-content/themes/unite2014/functions.php
+++ wp-content/themes/unite2014/functions.php
@@ -1,1 +1,9 @@
 <?php
+add_action( 'unite2014_footer', 'unite2014_footer_info', 30 );
+/**
+ * function to show the footer info, copyright information
+ */
+function unite2014_footer_info() {
+   $output = '<a href="###" title="Unite 2014">Unite 2014</a> powered by <a href="http://wordpress.org" title="WordPress" target="_blank">WordPress</a>.';
+   echo $output;
+}
